c124cec2ebdc6e8b39f206baf7d7b83663ec6640
Som1one <som1one@users.noreply.github.com>
2026-07-04 21:30:02 +0300

    fix: исправлен revision ID в миграции 0011

--- migrations/versions/0011_add_promoter_payment_details.py
+++ migrations/versions/0011_add_promoter_payment_details.py
@@ -7,8 +7,8 @@ from alembic import op
 import sqlalchemy as sa
 
 
-revision = "0011"
-down_revision = "0010"
+revision = "0011_promoter_payment_details"
+down_revision = "0010_payout_route_nullable"
 branch_labels = None
 depends_on = None
 
